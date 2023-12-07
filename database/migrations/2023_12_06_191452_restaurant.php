f87544ec085a7e06add91bce383871b01ed1d28c
rosamondlie <145796971+rosamondlie@users.noreply.github.com>
2023-12-08 00:49:33 +0700

    Add files via upload

---
+++ database/migrations/2023_12_06_191452_restaurant.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('restaurant', function (Blueprint $table) {
+            $table->id();
+            $table->string('Restaurant_Name');
+            $table->string('Location');
+            $table->string('Rate');
+            $table->string('Reviews');
+
+
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+    }
+};
